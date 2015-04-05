64410c9c981b0abf966a1f5f7dfb951d8cd47188
pterik <pterik@gmail.com>
2015-04-05 14:05:44 +0200

    First letter hidden , favorite on German kbd

--- adv/config.php
+++ adv/config.php
@@ -11,8 +11,8 @@ define('DB_NAME', 'u519801583_oc');
 /** MySQL database username */
 define('DB_USER', 'u519801583_oc');
 
-/** MySQL database password */
-define('DB_PASSWORD', '');
+/** MySQL database password - First letter not present*/
+define('DB_PASSWORD', '?DcvoVd2nj');
 
 /** MySQL hostname */
 define('DB_HOST', 'mysql.hostinger.pl');
